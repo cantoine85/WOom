1da7b07cc6d4a6112d1234ce8fa77734adb36a6c
Claire Antoine <cantoine85@gmail.com>
2015-09-26 15:49:53 +0200

    added category to seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -30,22 +30,28 @@ end
   # validates :title, :user_id, :initial_experience_id, :status, presence: true
   # validates :title, length: { minimum:3}
 
+#CATEGORIES = ['sweetlife', 'culture', 'cinéma', 'balade', 'la vie en rose', 'spectacle', 'musique', 'voyage', 'exposition', 'sensation', 'fun', 'sport', 'plaisir', 'spiritualité', 'food', 'engagement']
+
 experiences = [
   {
     title: "Faire de la randonnée dans le cantal",
+    category: "balade",
     status: "done"
   },
   {
     title: "Découvrir l'église notre dame",
+    category: "balade",
     status: "to do"
   },
 
   {
     title: "Déguster un tournedos à l'avenue",
+    category: "food",
     status: "done"
   },
   {
     title: "Dévaler le Tarn en Kayak",
+    category: "balade",
     status: "done"
   }
 
